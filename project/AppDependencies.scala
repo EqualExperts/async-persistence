61b992f01a9facf8786e9871dedf4f992bf69d9e
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-09-27 21:02:01 +0100

    remove mongo references
    mongo impl have been moved to async-mongo-persistence

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,7 @@ object AppDependencies {
     "com.equalexperts" %% "play-async" % "0.1.0-9-gd6b4071" % "provided",
     "org.julienrf" %% "play-json-derived-codecs" % "3.3",
     "com.gu" %% "scanamo" % "0.9.5",
-
-    "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0"
+    "ch.qos.logback" % "logback-classic" % "1.2.3"
   )
 
   trait TestDependencies {
@@ -20,8 +19,7 @@ object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "org.scalatest" %% "scalatest" % "3.0.3" % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-        "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % scope
+        "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope
       )
     }.test
   }
