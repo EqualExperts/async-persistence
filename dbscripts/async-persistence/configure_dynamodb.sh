31f6e66e99c4d2bccba67f14b9e761f66091d86a
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-09-28 20:13:00 +0100

    WIP update the table creation scripts

--- dbscripts/async-persistence/configure_dynamodb.sh
+++ dbscripts/async-persistence/configure_dynamodb.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-aws --endpoint-url=http://localhost:8000 dynamodb create-table --cli-input-json file://asynctaskcache.json
+aws --endpoint-url=http://localhost:8000 dynamodb create-table --cli-input-json file://asyncTaskCache.json
+
+aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification file://ttl.json
+## aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification Enabled=true,AttributeName=start
--- dbscripts/async-persistence/delete_dynamodb.sh
+++ dbscripts/async-persistence/delete_dynamodb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws --endpoint-url=http://localhost:8000 dynamodb delete-table --table-name asynctaskcache
+aws --endpoint-url=http://localhost:8000 dynamodb delete-table --table-name asyncTaskCache
