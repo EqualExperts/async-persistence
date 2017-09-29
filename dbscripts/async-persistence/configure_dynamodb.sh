bbcaf10be8ff1858bbf1d67454b0d36daf9a1a32
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-09-29 06:17:32 +0100

    pull ttl scripts out of table create
    Currently local dynamo does not support TTL but keep these here for reference

--- dbscripts/async-persistence/configure_dynamodb.sh
+++ dbscripts/async-persistence/configure_dynamodb.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
 aws --endpoint-url=http://localhost:8000 dynamodb create-table --cli-input-json file://asyncTaskCache.json
-
-aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification file://ttl.json
-## aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification Enabled=true,AttributeName=start
