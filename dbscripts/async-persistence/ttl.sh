a4bc582a631d8aa656547725e2393017b11b77c0
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-10-03 11:42:23 +0100

    remove ttl json

--- dbscripts/async-persistence/ttl.sh
+++ dbscripts/async-persistence/ttl.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification Enabled=true,AttributeName=expiry
-#aws --endpoint-url=http://localhost:8000 dynamodb update-time-to-live --table-name asyncTaskCache --time-to-live-specification file://ttl.json
