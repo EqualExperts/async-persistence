d454e0c695ada69fdcc1f9b09f19b7b34ec1bf5e
thoeni <thoeni@gmail.com>
2017-10-31 22:01:27 +0000

    Increasing memory to dynamodb for tests and changing base image for build

--- project/LocalDynamoDb.scala
+++ project/LocalDynamoDb.scala
@@ -3,14 +3,15 @@
 object LocalDynamoDb {
 
   import com.localytics.sbt.dynamodb.DynamoDBLocalKeys._
-  import sbt._
   import sbt.Keys._
+  import sbt._
 
   def apply(conf : Configuration) : Seq[Setting[_]] = {
     Seq(
       dynamoDBLocalDownloadDir := file("./dynamodb_local"),
       dynamoDBLocalPort := 8000,
       dynamoDBLocalSharedDB := true,
+      dynamoDBLocalHeapSize := Some(1024),
       startDynamoDBLocal := startDynamoDBLocal.dependsOn(compile in conf).value,
 
       test in conf := (test in conf).dependsOn(startDynamoDBLocal).value,
